316ad7bc5533337b2142a8ebe16672b7673cdbd6
Wolf-Martell Montwé <wolf@wolfmontwe.de>
2022-05-05 11:54:24 +0200

    Add propagation of jacocoTestReport call

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,6 +49,7 @@ tasks.named<Wrapper>("wrapper") {
 tasks.maybeCreate("build").dependsOn(gradle.includedBuilds.map { it.task(":build") })
 tasks.maybeCreate("check").dependsOn(gradle.includedBuilds.map { it.task(":check") })
 tasks.maybeCreate("clean").dependsOn(gradle.includedBuilds.map { it.task(":clean") })
+tasks.maybeCreate("jacocoTestReport").dependsOn(gradle.includedBuilds.map { it.task(":jacocoTestReport") })
 tasks.maybeCreate("wrapper").dependsOn(gradle.includedBuilds.map { it.task(":wrapper") })
 tasks.maybeCreate("dependencyUpdates").dependsOn(gradle.includedBuilds.map { it.task(":dependencyUpdates") })
 tasks.maybeCreate("versionCatalogUpdate").dependsOn(gradle.includedBuilds.map { it.task(":versionCatalogUpdate") })
