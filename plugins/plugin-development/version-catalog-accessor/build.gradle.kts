6d44fce141cd14c407646ee34992ad9fa76d69fa
Wolf-Martell Montwé <wolf@wolfmontwe.de>
2022-05-09 13:42:59 +0200

    Change dependencies to version catalog

--- plugins/plugin-development/version-catalog-accessor/build.gradle.kts
+++ plugins/plugin-development/version-catalog-accessor/build.gradle.kts
@@ -30,8 +30,8 @@ gradlePlugin {
 }
 
 dependencies {
-    implementation("com.squareup:kotlinpoet:1.10.2")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-toml:2.13.0")
+    implementation(libs.kotlinPoet)
+    implementation(libs.jacksonToml)
 }
 
 apiValidation {
