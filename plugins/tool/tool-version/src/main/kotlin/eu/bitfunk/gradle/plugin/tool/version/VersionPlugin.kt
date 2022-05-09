05a6a0369a812087a79c5444ed20988017caa0dc
Wolf-Martell Montwé <wolf@wolfmontwe.de>
2022-05-09 23:25:22 +0200

    Add VersionPlugin

--- plugins/tool/tool-version/src/main/kotlin/eu/bitfunk/gradle/plugin/tool/version/VersionPlugin.kt
+++ plugins/tool/tool-version/src/main/kotlin/eu/bitfunk/gradle/plugin/tool/version/VersionPlugin.kt
@@ -18,11 +18,57 @@
 
 package eu.bitfunk.gradle.plugin.tool.version
 
+import eu.bitfunk.gradle.plugin.tool.version.VersionContract.Generator
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.extra
 
-public class VersionPlugin : Plugin<Project> {
+public class VersionPlugin() : Plugin<Project>, VersionContract.Plugin {
+
+    public constructor(generator: Generator) : this() {
+        _generator = generator
+    }
+
+    private var _generator: Generator? = null
+
+    private fun getGenerator(project: Project): Generator {
+        return if (_generator == null)
+            VersionNameGenerator(project)
+        else {
+            _generator as Generator
+        }
+    }
 
     override fun apply(target: Project) {
+        val generator = getGenerator(target)
+
+        addPlugins(target)
+        configureVersion(target, generator)
+        configureVersionTasks(target, generator)
+    }
+
+    override fun addPlugins(project: Project): Unit = with(project) {
+        pluginManager.apply("eu.upwolf.git-version")
+    }
+
+    override fun configureVersion(project: Project, generator: Generator): Unit = with(project) {
+        allprojects {
+            version = generator.generateVersionName()
+            extra.set("versionCode", generator.generateVersionCode())
+            extra.set("versionCodeFeature", generator.generateFeatureVersionCode())
+        }
+    }
+
+    override fun configureVersionTasks(project: Project, generator: Generator): Unit = with(project) {
+        tasks.register("versionInfo") {
+            group = "versioning"
+
+            doLast {
+                println("VersionName: ${generator.generateVersionName()}")
+                println("VersionCode: ${generator.generateVersionCode()}")
+                println("VersionCodeFeature: ${generator.generateFeatureVersionCode()}")
+                println("VersionDetails: ${generator.generateVersionDetails()}")
+            }
+        }
     }
 }
