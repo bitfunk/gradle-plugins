37e79d38c6eb5cedb4ce68b368a065540c958af2
Wolf-Martell Montwé <wolf@wolfmontwe.de>
2022-11-10 19:24:03 +0100

    Add explicit convention plugin dependency to report

--- plugins/quality/report/build.gradle.kts
+++ plugins/quality/report/build.gradle.kts
@@ -16,8 +16,9 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    id("eu.bitfunk.gradle.plugin.development.convention")
+    alias(libsReport.plugins.gradlePluginConvention)
 }
 
 group = "eu.bitfunk.gradle.plugin.quality"
