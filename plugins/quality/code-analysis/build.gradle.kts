60c9b1b4c5a77213fb27860a9fccfbae765a95f8
Wolf-Martell Montwé <wolf@wolfmontwe.de>
2022-11-16 18:21:53 +0100

    Add version catalog accessor to code-analysis plugin

--- plugins/quality/code-analysis/build.gradle.kts
+++ plugins/quality/code-analysis/build.gradle.kts
@@ -19,7 +19,7 @@
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libsCodeAnalysis.plugins.gradlePluginConvention)
-    id("eu.bitfunk.gradle.plugin.development.version.catalog.accessor")
+    alias(libsCodeAnalysis.plugins.gradleVersionCatalogAccessor)
 }
 
 group = "eu.bitfunk.gradle.plugin.quality"
